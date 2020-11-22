8e82692bd30f101f7babb4f83cb867890efcc29e
Turing <turingksc@gmail.com>
2020-11-22 17:37:47 +0800

    .

--- DAFL/DAFL-train.py
+++ DAFL/DAFL-train.py
@@ -162,9 +162,9 @@ for epoch in range(opt.n_epochs):
         for i, (images, labels) in enumerate(data_test_loader):
             net.eval()
             output = net(images)
-            avg_loss += torch.nn.CrossEntropyLoss(output, labels, reduction='sum')
-            pred = output.data.max(1)[1]
-            total_correct += pred.eq(labels.data.view_as(pred)).sum()
+            avg_loss += F.cross_entropy(output, labels, reduction='sum').item()
+            pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
+            total_correct += pred.eq(labels.data.view_as(pred)).sum().item()
 
     len_data_test = 7936
     print('===========', avg_loss, total_correct)
